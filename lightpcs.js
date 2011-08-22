0983ce7b2da43b733ca94aa045e81ff5b26359ef
Raymond <raymond@lmws.net>
2011-08-22 00:31:34 -0700

    added timeout

--- lightpcs.js
+++ lightpcs.js
@@ -2,6 +2,7 @@ $('.album').each(function (index, album) {
   album = $(album);
   album.galleria({
     picasa: 'useralbum:' + album.data('user-album'),
+    imageTimeout: 300000,
     height: 500
   });
 });
